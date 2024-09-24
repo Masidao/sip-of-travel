015244d226cdafa6e6bd3abfc742a623433cdedf
imojaek <imojaek@gmail.com>
2024-09-24 17:18:44 +0900

    [feat/#6] chore: 테스트용 초기 회원데이터 추가

--- Back/src/main/resources/data.sql
+++ Back/src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO cities (name, latitude, longitude) VALUES
                                                    ('서울', 37.5665, 126.9780),
                                                    ('속초', 38.2064, 128.5912),
                                                    ('부산', 35.1796, 129.0756);
+
+INSERT INTO members (nickname, loginId, password) VALUES
+                                                   ('다오', 'dao', '1234'),
+                                                   ('마롱', 'marron', '1234');
