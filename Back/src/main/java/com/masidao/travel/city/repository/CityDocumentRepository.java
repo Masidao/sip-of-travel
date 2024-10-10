e06c631faeba19faece87d83a5e4891a48340f1c
imojaek <imojaek@gmail.com>
2024-10-10 13:39:47 +0900

    [feat/#6] feat: 엘라스틱서치 제거

--- Back/src/main/java/com/masidao/travel/city/repository/CityDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.masidao.travel.city.repository;
-
-import com.masidao.travel.city.entity.CityDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CityDocumentRepository extends ElasticsearchRepository<CityDocument, Long> {
-}
