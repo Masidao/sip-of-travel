35ca0e0bf253a5faf3012e7550285b5eb6323038
imojaek <imojaek@gmail.com>
2024-10-15 15:01:01 +0900

    [feat/#21] feat: 도시 데이터 반환 형식 변경

--- Back/src/main/java/com/masidao/travel/city/dto/CityListResponse.java
+++ Back/src/main/java/com/masidao/travel/city/dto/CityListResponse.java
@@ -5,7 +5,11 @@ import lombok.Builder;
 @Builder
 public record CityListResponse (
         Long id,
-        String name
+        String name,
+        String cityImage,
+        Double cityLatitude,
+        Double cityLongitude,
+        Long cityLevel
 ){
 
 }
