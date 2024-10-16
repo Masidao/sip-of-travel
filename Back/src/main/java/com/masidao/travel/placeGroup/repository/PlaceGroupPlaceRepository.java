7d4b035b4d2dd873b54c0d27a30c6e47a09ff118
imojaek <imojaek@gmail.com>
2024-10-16 15:02:04 +0900

    [feat/#16] feat: place, placeGroupPlace 리포지토리 추가

---
+++ Back/src/main/java/com/masidao/travel/placeGroup/repository/PlaceGroupPlaceRepository.java
@@ -0,0 +1,8 @@
+package com.masidao.travel.placeGroup.repository;
+
+import com.masidao.travel.placeGroup.entity.PlaceGroupPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceGroupPlaceRepository extends JpaRepository<PlaceGroupPlace, Long> {
+
+}
