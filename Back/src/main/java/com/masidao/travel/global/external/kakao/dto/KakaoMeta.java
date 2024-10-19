7074728e71aedce4158c5a28348dd63ebeab6ab4
imojaek <imojaek@gmail.com>
2024-10-19 09:46:57 +0900

    [feat/#16] style: 카카오맵 메타데이터의 주석 수정

--- Back/src/main/java/com/masidao/travel/global/external/kakao/dto/KakaoMeta.java
+++ Back/src/main/java/com/masidao/travel/global/external/kakao/dto/KakaoMeta.java
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record KakaoMeta(
 
         @JsonProperty("total_count")
-        Integer totalCount,
+        Integer totalCount, // 해당 키워드로 검색한 결과로 나온 장소의 총 개수
 
         @JsonProperty("pageable_count")
-        Integer pageableCount,  //
+        Integer pageableCount,  // 출력 가능한 장소의 총 개수. (중복제거 등의 이유로 줄어든 데이터)
 
         @JsonProperty("is_end")
         Boolean isEnd,            // 현재 페이지가 마지막 페이지인지 여부
