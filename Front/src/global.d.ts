c475540f621c67a069df3c74c1876558986ebc23
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-11-04 17:28:28 +0900

    ✨ feat: services 라이브러리 추가 - kakao 속성 추가

--- Front/src/global.d.ts
+++ Front/src/global.d.ts
@@ -3,6 +3,20 @@ interface Window {
     maps: {
       LatLng: new (lat: number, lng: number) => any;
       Map: new (container: HTMLElement, options: any) => any;
+      LatLngBounds: new () => any;
+      services: {
+        Places: new () => {
+          keywordSearch: (
+            keyword: string,
+            callback: (data: any, status: string, pagination: any) => void
+          ) => void;
+        };
+        Status: {
+          OK: string;
+          ZERO_RESULT: string;
+          ERROR: string;
+        };
+      };
     };
   };
 }
